c4c23f9c5fab1c7d0e1a251b51a1d38728a80394
Stanislas Durand <standudu7@gmail.com>
2022-02-02 19:23:49 +0100

    basic refactoring

---
+++ java/src/main/java/GamePlayer.java
@@ -0,0 +1,60 @@
+
+public class GamePlayer {
+    private int d1;
+    private int d2;
+    private int d3;
+    private int d4;
+    private int d5;
+
+    public GamePlayer(int d1, int d2, int d3, int d4, int d5) {
+        this.d1 = d1;
+        this.d2 = d2;
+        this.d3 = d3;
+        this.d4 = d4;
+        this.d5 = d5;
+    }
+
+    public int[] numToArrays(){
+        return new int[]{ d1, d2, d3, d4, d5 };
+    }
+
+    public int getD1() {
+        return d1;
+    }
+
+    public void setD1(int d1) {
+        this.d1 = d1;
+    }
+
+    public int getD2() {
+        return d2;
+    }
+
+    public void setD2(int d2) {
+        this.d2 = d2;
+    }
+
+    public int getD3() {
+        return d3;
+    }
+
+    public void setD3(int d3) {
+        this.d3 = d3;
+    }
+
+    public int getD4() {
+        return d4;
+    }
+
+    public void setD4(int d4) {
+        this.d4 = d4;
+    }
+
+    public int getD5() {
+        return d5;
+    }
+
+    public void setD5(int d5) {
+        this.d5 = d5;
+    }
+}
--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -1,20 +1,25 @@
-public class Yatzy {
+import java.util.Arrays;
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
-    {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+public class Yatzy {
+    private GamePlayer gamePlayer;
+
+    private Yatzy(GamePlayer gamePlayer) {
+        this.gamePlayer = gamePlayer;
     }
 
-    public static int yatzy(int... dice)
+    public static Yatzy initGamePlayer(int... dice){
+        return new Yatzy(new GamePlayer(dice[0], dice[1], dice[2], dice[3], dice[4]));
+    }
+
+    /* Methodes */
+    public int chance() {
+        return Arrays.stream(gamePlayer.numToArrays()).sum();
+    }
+
+    public int yatzy()
     {
         int[] counts = new int[6];
-        for (int die : dice)
+        for (int die : this.gamePlayer.numToArrays())
             counts[die-1]++;
         for (int i = 0; i != 6; i++)
             if (counts[i] == 5)
@@ -22,54 +27,43 @@ public class Yatzy {
         return 0;
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
+    public int ones() {
         int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
+        if (this.gamePlayer.getD1() == 1) sum++;
+        if (this.gamePlayer.getD2() == 1) sum++;
+        if (this.gamePlayer.getD3() == 1) sum++;
+        if (this.gamePlayer.getD4() == 1) sum++;
+        if (this.gamePlayer.getD5() == 1)
             sum++;
 
         return sum;
     }
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
+    public int twos() {
         int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
+        if (this.gamePlayer.getD1() == 2) sum += 2;
+        if (this.gamePlayer.getD2() == 2) sum += 2;
+        if (this.gamePlayer.getD3() == 2) sum += 2;
+        if (this.gamePlayer.getD4() == 2) sum += 2;
+        if (this.gamePlayer.getD5() == 2) sum += 2;
         return sum;
     }
 
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;    
+    public int threes() {
+        int s;
         s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
+        if (this.gamePlayer.getD1() == 3) s += 3;
+        if (this.gamePlayer.getD2() == 3) s += 3;
+        if (this.gamePlayer.getD3() == 3) s += 3;
+        if (this.gamePlayer.getD4() == 3) s += 3;
+        if (this.gamePlayer.getD5() == 3) s += 3;
         return s;
-    }
-
-    protected int[] dice;
-    public Yatzy(int d1, int d2, int d3, int d4, int _5)
-    {
-        dice = new int[5];
-        dice[0] = d1;
-        dice[1] = d2;
-        dice[2] = d3;
-        dice[3] = d4;
-        dice[4] = _5;
     }
 
     public int fours()
     {
-        int sum;    
-        sum = 0;
+        int sum = 0;
+        int[] dice = this.gamePlayer.numToArrays();
         for (int at = 0; at != 5; at++) {
             if (dice[at] == 4) {
                 sum += 4;
@@ -82,8 +76,8 @@ public class Yatzy {
     {
         int s = 0;
         int i;
-        for (i = 0; i < dice.length; i++) 
-            if (dice[i] == 5)
+        for (i = 0; i < this.gamePlayer.numToArrays().length; i++)
+            if (this.gamePlayer.numToArrays()[i] == 5)
                 s = s + 5;
         return s;
     }
@@ -91,20 +85,20 @@ public class Yatzy {
     public int sixes()
     {
         int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
+        for (int at = 0; at < this.gamePlayer.numToArrays().length; at++)
+            if (this.gamePlayer.numToArrays()[at] == 6)
                 sum = sum + 6;
         return sum;
     }
 
-    public static int score_pair(int d1, int d2, int d3, int d4, int d5)
+    public int score_pair()
     {
         int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
+        counts[this.gamePlayer.getD1()-1]++;
+        counts[this.gamePlayer.getD2()-1]++;
+        counts[this.gamePlayer.getD3()-1]++;
+        counts[this.gamePlayer.getD4()-1]++;
+        counts[this.gamePlayer.getD5()-1]++;
         int at;
         for (at = 0; at != 6; at++)
             if (counts[6-at-1] >= 2)
@@ -112,66 +106,66 @@ public class Yatzy {
         return 0;
     }
 
-    public static int two_pair(int d1, int d2, int d3, int d4, int d5)
+    public int two_pair()
     {
         int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
+        counts[this.gamePlayer.getD1()-1]++;
+        counts[this.gamePlayer.getD2()-1]++;
+        counts[this.gamePlayer.getD3()-1]++;
+        counts[this.gamePlayer.getD4()-1]++;
+        counts[this.gamePlayer.getD5()-1]++;
         int n = 0;
         int score = 0;
         for (int i = 0; i < 6; i += 1)
             if (counts[6-i-1] >= 2) {
                 n++;
                 score += (6-i);
-            }        
+            }
         if (n == 2)
             return score * 2;
         else
             return 0;
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
+    public int four_of_a_kind()
     {
         int[] tallies;
         tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
+        tallies[gamePlayer.getD1()-1]++;
+        tallies[gamePlayer.getD2()-1]++;
+        tallies[gamePlayer.getD3()-1]++;
+        tallies[gamePlayer.getD4()-1]++;
+        tallies[gamePlayer.getD5()-1]++;
         for (int i = 0; i < 6; i++)
             if (tallies[i] >= 4)
                 return (i+1) * 4;
         return 0;
     }
 
-    public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
+    public int three_of_a_kind()
     {
         int[] t;
         t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
+        t[gamePlayer.getD1()-1]++;
+        t[gamePlayer.getD2()-1]++;
+        t[gamePlayer.getD3()-1]++;
+        t[gamePlayer.getD4()-1]++;
+        t[gamePlayer.getD5()-1]++;
         for (int i = 0; i < 6; i++)
             if (t[i] >= 3)
                 return (i+1) * 3;
         return 0;
     }
 
-    public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
+    public int smallStraight()
     {
         int[] tallies;
         tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
+        tallies[gamePlayer.getD1()-1] += 1;
+        tallies[gamePlayer.getD2()-1] += 1;
+        tallies[gamePlayer.getD3()-1] += 1;
+        tallies[gamePlayer.getD4()-1] += 1;
+        tallies[gamePlayer.getD5()-1] += 1;
         if (tallies[0] == 1 &&
             tallies[1] == 1 &&
             tallies[2] == 1 &&
@@ -181,15 +175,15 @@ public class Yatzy {
         return 0;
     }
 
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
+    public int largeStraight()
     {
         int[] tallies;
         tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
+        tallies[gamePlayer.getD1()-1] += 1;
+        tallies[gamePlayer.getD2()-1] += 1;
+        tallies[gamePlayer.getD3()-1] += 1;
+        tallies[gamePlayer.getD4()-1] += 1;
+        tallies[gamePlayer.getD5()-1] += 1;
         if (tallies[1] == 1 &&
             tallies[2] == 1 &&
             tallies[3] == 1 &&
@@ -199,7 +193,7 @@ public class Yatzy {
         return 0;
     }
 
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
+    public int fullHouse()
     {
         int[] tallies;
         boolean _2 = false;
@@ -208,15 +202,12 @@ public class Yatzy {
         boolean _3 = false;
         int _3_at = 0;
 
-
-
-
         tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
+        tallies[gamePlayer.getD1()-1] += 1;
+        tallies[gamePlayer.getD2()-1] += 1;
+        tallies[gamePlayer.getD3()-1] += 1;
+        tallies[gamePlayer.getD4()-1] += 1;
+        tallies[gamePlayer.getD5()-1] += 1;
 
         for (i = 0; i != 6; i += 1)
             if (tallies[i] == 2) {
@@ -236,6 +227,3 @@ public class Yatzy {
             return 0;
     }
 }
-
-
-
